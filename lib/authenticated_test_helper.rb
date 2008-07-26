72f3ccb39ea5673cb43ccccd07b0d6277c5d10e7
Evan Dorn <evan@PBG4.gateway.2wire.net>
2008-07-26 02:48:00 -0700

    wrote simple home view and tests

--- lib/authenticated_test_helper.rb
+++ lib/authenticated_test_helper.rb
@@ -4,6 +4,13 @@ module AuthenticatedTestHelper
     @request.session[:user_id] = user ? users(user).id : nil
   end
 
+  def logout
+    @request.session[:user_id] = nil
+    if defined?(controller)
+      controller.stub!(:current_user).and_return(:false)
+    end
+  end  
+  
   def authorize_as(user)
     @request.env["HTTP_AUTHORIZATION"] = user ? ActionController::HttpAuthentication::Basic.encode_credentials(users(user).login, 'monkey') : nil
   end
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,12 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "/home/index" do
+
+describe "/home/index when logged in" do
+  fixtures        :users
+  
   before(:each) do
+    login_as :quentin
     render 'home/index'
   end
   
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/home/index])
+    response.should have_tag('p', %r[You are logged in.])
+  end
+  
+  it "should link to logout page" do
+    response.should have_tag("a[href=?]", logout_path)
+  end  
+  
+end
+
+describe "/home/index when logged out" do
+  fixtures        :users  
+  before(:each) do
+    render 'home/index'
+  end
+  
+  #Delete this example and add some real ones or delete this file
+  it "should say you are logged out" do
+    response.should have_tag('p', %r[You are not logged in.])
+  end
+
+  it "should link to logout action" do
+    response.should have_tag("a[href=?]", login_path)
   end
 end
